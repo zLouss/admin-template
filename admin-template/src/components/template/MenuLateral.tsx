50b801c85afa0d0af17af5f69fa90c4884365fea
luisbelo <luis.victor.belo@gmail.com>
2024-01-09 13:01:20 -0300

    Atualização 09/01

---
+++ admin-template/src/components/template/MenuLateral.tsx
@@ -0,0 +1,13 @@
+import { IconeAjustes, IconeCasa, IconeSino } from "../icons"
+import MenuItem from "./MenuItem"
+export default function MenuLateral() {
+    return (
+        <aside>
+            <ul>
+                <MenuItem url="/" texto="Início" icone={IconeCasa} />
+                <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
+                <MenuItem url="/notificacoes" texto="Notificações" icone={IconeSino} />
+            </ul>
+        </aside>
+    )
+}
